7cce22c95ca5059a5e04a5806d35541d77596f18
= <nomani.zain@gmail.com>
2025-03-27 17:43:13 +0000

    experiment and main edited: Almost ready for AI

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ Test by running "streamlit run main.py" in terminal to test
 """
 import streamlit as st
 from Surah import Surah
-x: int = 2
+x: int = st.slider("Select a Chapter", min_value = 1, max_value = 114, value = 1)
+
+
 st.title(f"Chapter {x}")
 surah: dict = Surah().getSurah(x)
+
+
+
 for index, item in enumerate(surah):
-    st.title(str(index) + " " + item["Arabic"])
-    st.write(item["English"])
+    st.divider()
+    col1, col2 = st.columns([5, 2])
+    with col2:
+        if st.button(f"Query verse {index + 1}", key = index):
+            st.write("Button clicked")
+    
+    with col1:
+        arabic = item["Arabic"]
+        st.title(arabic)
+        st.write(item["English"])
